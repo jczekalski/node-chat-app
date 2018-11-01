9abefe34e3d86c1f2884a99fc94fe80a6434d4d5
John Czekalski <john.czekalski@gmail.com>
2018-11-01 17:27:48 +0100

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+const socket = io();
+
+socket.on('connect', function () {
+  console.log('Connected to server');
+
+  socket.emit('createMessage', {
+    from: 'jen@example.com',
+    text: 'Hey, this is Jen'
+  })
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server')
+});
+
+socket.on('newMessage', function(message) {
+  console.log('New message', message);
+});
